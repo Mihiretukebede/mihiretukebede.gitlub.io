4d73810cc434ab455b7c3d9e3177e67ddece2f5d
Mihiretu <mihiretaabush@gmail.com>
2020-06-30 23:26:58 +0200

    new blog

--- stepcount.R
+++ stepcount.R
@@ -1,14 +1,4 @@
----
-title: "Blogs"
----
 
-<link rel="stylesheet" href="styles.css" type="text/css">
-
-# Animating Bargraph
-
-  Hello, this is my first ever blog post. Detail explanations of this data vizualization and other upcoming blogs are coming soon soon! Stay tuned. 
-
-```{r}
 library(readxl)
 mystepcount <- read_excel("mystepcount.xlsx")
 
@@ -35,9 +25,11 @@ animate(
         axis.title = element_text(size=16, face="bold")) + 
   xlab("September") +  
   scale_y_discrete(name="Step counts", limits=c(5000,10000, 15000, 20000, 25000, 30000)) +
-  theme(axis.line = element_line(colour = "black"),
+  
+    theme(axis.line = element_line(colour = "black"),
           panel.border = element_blank()) ,
   
   duration = .001
   )
-```
+
+
